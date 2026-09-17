685ebe40650bd307aeed88e07639846361c365db
Ayush Raj <mailtoayushh@gmail.com>
2026-09-17 17:44:43 +0530

    Configure corpus admin

--- corpus/admin.py
+++ corpus/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Subject, SourceDocument, Chunk
 
-# Register your models here.
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+
+@admin.register(SourceDocument)
+class SourceDocumentAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "subject", "created_at")
+    list_filter = ("subject",)
+    search_fields = ("title",)
+    ordering = ("-created_at",)
+
+@admin.register(Chunk)
+class ChunkAdmin(admin.ModelAdmin):
+    list_display = ("id", "document", "chunk_index")
+    list_filter = ("document",)
+    search_fields = ("content",)
+    ordering = ("document", "chunk_index")
